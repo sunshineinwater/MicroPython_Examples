9c09538e7f9c8c4e43c48024c610a74dc4b7b293
sunshineinwater <307080785@qq.com>
2021-08-19 19:15:43 +0800

    --

--- pyWiFi-ESP32/2.传感器实验/9.MPU6050/main.py
+++ pyWiFi-ESP32/2.传感器实验/9.MPU6050/main.py
@@ -1,7 +1,7 @@
 # 在这里写上你的代码 :-)
-from machine import I2C,Pin
+from machine import SoftI2C,Pin
 import mpu6050,time
-i2c = I2C(scl=Pin(17), sda=Pin(16))
+i2c = SoftI2C(scl=Pin(4), sda=Pin(7))
 accelerometer = mpu6050.accel(i2c)
 
 while True:
